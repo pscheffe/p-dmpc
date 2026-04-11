6786062b5786dc28bc076708953f88fe2bb10cf7
Patrick Scheffe <patrick.scheffe@rwth-aachen.de>
2026-04-11 13:56:22 +0200

    [hlc] prepare mex implementation of mcts

---
+++ hlc/optimizer/graph_search/build_mcts_expand_loop_mex.m
@@ -0,0 +1,20 @@
+function build_mcts_expand_loop_mex()
+    % BUILD_MCTS_EXPAND_LOOP_MEX  Compile the MCTS loop MEX prototype.
+    %
+    % This compiles the loop-only MEX used by mcts_expand_loop_gateway.
+
+    source_file = fullfile(fileparts(mfilename('fullpath')), 'mcts_expand_loop_mex.cpp');
+
+    if ~isfile(source_file)
+        error('build_mcts_expand_loop_mex:MissingSource', ...
+            'Missing source file: %s', source_file);
+    end
+
+    current_folder = pwd;
+    cleanup = onCleanup(@() cd(current_folder));
+    cd(fileparts(source_file));
+
+    mex('-R2018a', 'CXXFLAGS=$CXXFLAGS -std=c++17', 'mcts_expand_loop_mex.cpp');
+
+    clear cleanup;
+end
